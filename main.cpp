2cdec2d2a851ac0864a49bd4928d51eda78a775c
GABRIEL SINKHAMBA <bsc-com-08-23@unima.ac.mw>
2025-04-04 10:41:01 +0200

    Added adition features

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@ using namespace std;
  
 int main(){
     cout << "Helloo, World!"<<endl;
+    //add new features to the code 
+    int a, b;
+    cout<<"Enter numbers"<<endl;
+    cin >>a >>b;
+    cout<< "The sum is: "<< (a+b)<<endl;
     return 0;
 }
